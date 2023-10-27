2c7c742bf802cbb51e8c2e4c2a945d5023c33511
khoi3008 <135519953+khoi3008@users.noreply.github.com>
2023-10-27 12:20:02 +0700

    Update page.tsx

--- src/app/products-detail/[index]/page.tsx
+++ src/app/products-detail/[index]/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-
-import { DataType, userGlobalContext } from "@/app/context/Store";
-import { useEffect, useState, FC } from "react";
-interface pageProps {
-  params: { index: number };
+function Detail() {
+  return ( <>
+    Detail
+  </>  );
 }
 
-const PAGE: FC<pageProps> = ({ params }) => {
-  const { data } = userGlobalContext();
-  const [dataone, setdataone] = useState<DataType>();
-
-  useEffect(() => {
-    const searchData = data.find((item) => item.id == params.index);
-    setdataone(searchData);
-  }, [params.index]);
-
-  return (
-    <h1>trang detail</h1> 
-  );
-};
-
-export default PAGE;
+export default Detail;
